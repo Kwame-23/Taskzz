16a0fb7660ed1845aef0291bb5b13b9b67b50497
Kwame-23 <137163770+Kwame-23@users.noreply.github.com>
2024-09-03 16:27:15 +0000

    progress so far

--- settings/connection.php
+++ settings/connection.php
@@ -1,8 +1,8 @@
 <?php
 
 define('DB_HOST', 'localhost');
-define('DB_USER', 'kwame');
-define('DB_PASS', 'Kwame23');
+define('DB_USER', 'root');
+define('DB_PASS', '');
 define('DB_NAME', 'Todozz');
 
 // Connect to the database using mysqli
