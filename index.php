0836c6caad36eba8f7bfc042bf79e59e007c299d
Kwame-23 <137163770+Kwame-23@users.noreply.github.com>
2024-09-03 11:34:51 +0000

    progress so far

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Notepad Web App</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <h1>Notepad Web App</h1>
+    <p>Welcome to the Notepad web app, where you can create, save, and view your notes.</p>
+    <a href="login/login.php">Login</a> | <a href="gitlogin/register.php">Register</a>
+</body>
+</html>
