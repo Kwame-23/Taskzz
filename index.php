c5878b8f2efce0b8d6912ee24a211bd46b14be69
Kwame-23 <137163770+Kwame-23@users.noreply.github.com>
2024-09-03 11:41:25 +0000

    progress so far

--- index.php
+++ index.php
@@ -11,6 +11,6 @@
 <body>
     <h1>Notepad Web App</h1>
     <p>Welcome to the Notepad web app, where you can create, save, and view your notes.</p>
-    <a href="login/login.php">Login</a> | <a href="gitlogin/register.php">Register</a>
+    <a href="login/login.php">Login</a> | <a href="login/register.php">Register</a>
 </body>
 </html>
