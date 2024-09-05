515bee90e7e2ad5f2be641d0734ee0caa4e86205
Malottey1 <malcolm.clottey@ashesi.edu.gh>
2024-09-05 12:43:08 +0000

    Squashed commit of the following:
    commit 2b85397186283eba4e0253226181508d85892958
    Author: Kwame-23 <137163770+Kwame-23@users.noreply.github.com>
    Date:   Thu Sep 5 05:19:34 2024 +0000
    
        front end changes
    
    commit a1865f7b3cc97f0bde9fc09d397e5641cb7ecf0b
    Author: Kwame-23 <137163770+Kwame-23@users.noreply.github.com>
    Date:   Wed Sep 4 12:00:51 2024 +0000
    
        new changes

--- actions/add_task.php
+++ actions/add_task.php
@@ -7,27 +7,26 @@ if (isset($_POST['addTask'])) {
     $userID = $_SESSION['user_id'];
     $taskName = trim($_POST['taskName']);
     $description = trim($_POST['description']);
-    $listID = 2; //(int)$_GET['id'];
+    $listID = $_GET['project_id'];
 
     if (empty($taskName)) {
-        header("Location: ../view/tasks.php?msg=Task must be given a name!");
+        header("Location: ../view/tasks.php?project_id=" . urlencode($listID) . "&msg=" . urlencode("Task must be given a name!"));
         exit();
     }
     $completed = false; 
 
-    $sql = "INSERT INTO tasks (list_id, description, name, completed) VALUES (?, ?, ?, ?)";
+    $sql = "INSERT INTO tasks (project_id, description, name, completed) VALUES (?, ?, ?, ?)";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("issi", $listID, $description, $taskName, $completed);
 
     if ($stmt->execute()) {
-        header("Location: ../view/tasks.php?msg=Task added successfully");
+        header("Location: ../view/tasks.php?project_id=" . urlencode($listID) . "&msg=" . urlencode("Task added successfully"));
         exit();
     } else {
-        //error_log("Task insertion error: " . $conn->error);
-        header("Location: ../view/tasks.php?msg=Error adding task, please try again.");
+        header("Location: ../view/tasks.php?project_id=" . urlencode($listID) . "&msg=" . urlencode("Error adding task, please try again."));
         exit();
     }
 } else {
-    header("Location: ../view/task.php");
+    header("Location: ../view/tasks.php?project_id=" . urlencode($listID)  . "&msg=" . urlencode("Error adding task, please try again."));
     exit();
 }
--- login/register.php
+++ login/register.php
@@ -3,16 +3,104 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registration</title>
-    <link rel="stylesheet" href="../css/register.css">
+    <title>Login</title>
+    <style>
+        *{
+            font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+            box-sizing: border-box;
+            margin: 0;
+            padding: 0;
+        }
+
+        body {
+            background-color: #ffffff; 
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
+
+        .container {
+            text-align: center;
+            background-color: #ffffff; 
+            padding: 30px; 
+            border: 2px solid #32CD32; 
+            border-radius: 15px;
+            box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+            width: 90%;
+            max-width: 400px;
+        }
+
+        .logo {
+            width: 120px;
+            height: 70px;
+            margin-bottom: 20px;
+        }
+
+        h1 {
+            color: #020000;
+            font-size: 24px;
+            margin: 10px 0;
+        }
+
+        .registration-form {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+
+        label {
+            margin-bottom: 5px;
+        }
+
+        input[type="text"], input[type="email"], input[type="password"] {
+            width: 100%; 
+            padding: 10px;
+            margin-bottom: 15px;
+            background-color: #f7f7f7; 
+            border: none; 
+            border-radius: 15px;
+            outline: none;
+            font-size: 16px;
+        }
+
+        input[type="text"]:focus, input[type="firstName"]:focus, input[type="lastName"]:focus, input[type="email"]:focus, input[type="password"]:focus, input[type="confirmPassword"]:focus {
+            border-color: #2980b9;
+        }
+
+        button {
+            background-color: #4CAF50;
+            color: #fff;
+            border: none;
+            padding: 10px 20px;
+            border-radius: 5px;
+            font-size: 16px;
+            cursor: pointer;
+        }
+
+        button:hover {
+            background-color: #3e8e41;
+        }
+
+        @media only screen and (max-width: 768px) {
+            .container {
+                padding: 20px;
+            }
+        }
+
+        @media only screen and (max-width: 480px) {
+            .container {
+                padding: 10px;
+            }
+        }
+    </style>
 </head>
 <body>
-
+    <img src="../images/logo.jpg" alt="Logo" class="logo"> 
     <div class="container">
         <div>
-            <h1>Sign Up</h1>
-            <hr style="width: 50%; color: #000000;">
-            <hr style="width: 63%; margin-bottom: 5%; margin-top: 2%;">
+            <h1 style="margin-bottom: 5%;">Hey! Sign in now</h1>
         </div>
         <form id="register-form" class="registration-form" action="../actions/register_action.php" method="POST" required> 
             <input type="text" name="firstName" id="firstName" placeholder="First Name" required>
@@ -21,8 +109,6 @@
             <input type="password" name="password" id="password" placeholder="Enter your password" required>
             <input type="password" name="confirmPassword" id="confirmPassword" placeholder="Re-enter your password" required>
 
-            <hr style="width: 63%; margin-top: 2%;">
-            <hr style="width: 50%; color: #000000;">
             
             <button type="submit" name="register">Register</button>
             <p>Already have an account? <a href="login.php">Login</a></p>
